b65d166fb4981db586ebffbbfaba739a7d03e5bc
matgeroina <157189615+matgeroina@users.noreply.github.com>
2026-02-18 13:39:24 -0600

    routes pages navigations!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 import './App.css'
+import { HashRouter, Routes, Route} from 'react-router'
+
+const Home = () => <h1>home</h1>;
+const Setup = () => <h1>setup</h1>;
+const PlayPage = () => <h1>play page</h1>;
+
+
 
 const App = () => {
 
   return (
 
     <div> 
-      Cleared out
+      <HashRouter>
+        <Routes>
+          <Route 
+          path='/'
+          element={
+            <Home />
+          }
+          />
+          <Route 
+          path='/setup'
+          element={
+            <Setup />
+          }
+          />
+          <Route 
+          path='/PlayPage'
+          element={
+           <PlayPage />
+          }
+          />
+          
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
