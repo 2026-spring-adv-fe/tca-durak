4e8d85799ddc3af95781ebca3daec8be0ee9fba9
matgeroina <157189615+matgeroina@users.noreply.github.com>
2026-04-01 13:00:36 -0500

    Revert "eee"
    This reverts commit de256953c347d7b10ce4de6df166895a3a51cafd.

--- src/App.tsx
+++ src/App.tsx
@@ -23,16 +23,10 @@ const dummyGameResults: GameResult[] = [
   },
 ];
 
-type Player = {
-  name: string;
-  checked: boolean;
-};
-
 const App = () => {
   const [gameResults, setGameResults] = useState<GameResult[]>(dummyGameResults);
   const [title, setTitle] = useState(APP_TITLE);
   const [theme, setTheme] = useState("light");
-  const [currentPlayers, setCurrentPlayers] = useState<Player[]>([]);
 
   const addNewGameResult = (gameResult: GameResult) =>
     setGameResults([
@@ -107,17 +101,7 @@ const App = () => {
             />
             <Route
               path="/setup"
-<<<<<<< Updated upstream
               element={<Setup setTitle={setTitle} />}
-=======
-              element={
-                <Setup
-                  setTitle={setTitle}
-                  previousPlayers={getPreviousPlayers(gameResults)}
-                  setCurrentPlayers={setCurrentPlayers}
-                />
-              }
->>>>>>> Stashed changes
             />
             <Route
               path="/playpage"
