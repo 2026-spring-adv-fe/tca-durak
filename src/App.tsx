89ceab5ef62be5c600285fd112359c1cd92e4bcf
matgeroina <157189615+matgeroina@users.noreply.github.com>
2026-05-04 13:35:10 -0500

    eh

--- src/App.tsx
+++ src/App.tsx
@@ -93,42 +93,41 @@ const App = () => {
     getAvgGameDurationsByPlayerCount(gameResults);
   const gameCountByMonthData = getGameCountByMonth(gameResults);
 
-  function getReverseChrongameData(gameResults: GameResult[]): { date: string; duration: string; players: string; }[] {
-    throw new Error("Function not implemented.");
-  }
-  function getReverseChrongameData(gameResults: GameResult[]): {
-    date: string;
-    duration: string;
-    players: string;
-  }[] {
-    return [...gameResults]
-      .sort(
-        (a, b) =>
-          new Date(b.start).getTime() - new Date(a.start).getTime()
-      )
-      .map(({ start, end, players }) => {
-        const startDate = new Date(start);
-        const endDate = new Date(end);
-        const durationMinutes = Math.round(
-          (endDate.getTime() - startDate.getTime()) / 60000
-        );
-        const hours = Math.floor(durationMinutes / 60);
-        const minutes = durationMinutes % 60;
+function getReverseChrongameData(gameResults: GameResult[]): {
+  date: string;
+  duration: string;
+  players: string;
+}[] {
+  return [...gameResults]
+    .sort(
+      (a, b) =>
+        new Date(b.start).getTime() - new Date(a.start).getTime()
+    )
+    .map(({ start, end, players }) => {
+      const startDate = new Date(start);
+      const endDate = new Date(end);
 
-        return {
-          date: startDate.toLocaleDateString("pl-PL", {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-          }),
-          duration:
-            hours > 0
-              ? `${hours}h ${minutes.toString().padStart(2, "0")}m`
-              : `${minutes}m`,
-          players: players.join(", "),
-        };
-      });
-  }
+      const durationMinutes = Math.round(
+        (endDate.getTime() - startDate.getTime()) / 60000
+      );
+
+      const hours = Math.floor(durationMinutes / 60);
+      const minutes = durationMinutes % 60;
+
+      return {
+        date: startDate.toLocaleDateString("pl-PL", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        }),
+        duration:
+          hours > 0
+            ? `${hours}h ${minutes.toString().padStart(2, "0")}m`
+            : `${minutes}m`,
+        players: players.join(", "),
+      };
+    });
+}
 
   return (
     <div className="min-h-screen" data-theme={theme}>
